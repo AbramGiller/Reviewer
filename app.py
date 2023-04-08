fcf114346b997151ac3a6c072f3a5fcb9574dc3d
Abram Giller <A.Giller@sms.ed.ac.uk>
2023-04-08 23:54:55 +0100

    Updated GPT analysis and added result saving

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from pdf_processing import extract_text_from_pdf, preprocess_text
 from gpt_integration import review_paper_gpt3
 from post_processing import extract_key_points
+
 def main():
     # Read and preprocess the PDF file
     pdf_path = '/Users/s2222119/Reviewer4/CRISPR.pdf'
@@ -8,17 +9,26 @@ def main():
     preprocessed_chunks = preprocess_text(extracted_text)
 
     # Iterate through the chunks and send them to GPT-3 for review
-    for chunk in preprocessed_chunks:
-        review = review_paper_gpt3(chunk)
+    with open("output.txt", "a") as output_file:
+        for chunk in preprocessed_chunks:
+            review = review_paper_gpt3(chunk)
 
-        # Extract key points from the GPT-3 review
-        key_points = extract_key_points(review)
+            # Extract key points from the GPT-3 review
+            key_points = extract_key_points(review)
 
-        # Print the results
-        print("Summary:", key_points["summary"])
-        print("Strengths:", ", ".join(key_points["strengths"]))
-        print("Weaknesses:", ", ".join(key_points["weaknesses"]))
-        print("\n")
+            # Write the results to the output file
+            output_file.write("Summary:\n")
+            output_file.write(key_points["summary"] + "\n")
+            output_file.write("Strengths:\n")
+            output_file.write(", ".join(key_points["strengths"]) + "\n")
+            output_file.write("Weaknesses:\n")
+            output_file.write(", ".join(key_points["weaknesses"]) + "\n\n")
+
+            # Print the results
+            print("Summary:", key_points["summary"])
+            print("Strengths:", ", ".join(key_points["strengths"]))
+            print("Weaknesses:", ", ".join(key_points["weaknesses"]))
+            print("\n")
 
 if __name__ == "__main__":
     main()

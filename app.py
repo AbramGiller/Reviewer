faf76a7744bf973f6b7eba44897ace4e5834c40a
Abram Giller <A.Giller@sms.ed.ac.uk>
2023-04-09 16:57:29 +0100

    I forgot

--- app.py
+++ app.py
@@ -8,27 +8,29 @@ def main():
     extracted_text = extract_text_from_pdf(pdf_path)
     preprocessed_chunks = preprocess_text(extracted_text)
 
+    # Initialize lists for storing all strengths and weaknesses
+    all_strengths = []
+    all_weaknesses = []
+
     # Iterate through the chunks and send them to GPT-3 for review
+    for chunk in preprocessed_chunks:
+        review = review_paper_gpt3(chunk)
+
+        # Extract key points from the GPT-3 review
+        key_points = extract_key_points(review)
+
+        # Add the strengths and weaknesses to their respective lists
+        all_strengths.extend(key_points["strengths"])
+        all_weaknesses.extend(key_points["weaknesses"])
+
+    # Save the results to a text file
     with open("output.txt", "a") as output_file:
-        for chunk in preprocessed_chunks:
-            review = review_paper_gpt3(chunk)
-
-            # Extract key points from the GPT-3 review
-            key_points = extract_key_points(review)
-
-            # Write the results to the output file
-            output_file.write("Summary:\n")
-            output_file.write(key_points["summary"] + "\n")
-            output_file.write("Strengths:\n")
-            output_file.write(", ".join(key_points["strengths"]) + "\n")
-            output_file.write("Weaknesses:\n")
-            output_file.write(", ".join(key_points["weaknesses"]) + "\n\n")
-
-            # Print the results
-            print("Summary:", key_points["summary"])
-            print("Strengths:", ", ".join(key_points["strengths"]))
-            print("Weaknesses:", ", ".join(key_points["weaknesses"]))
-            print("\n")
+        output_file.write("Summary:\n")
+        output_file.write(key_points["summary"] + "\n")
+        output_file.write("Strengths:\n")
+        output_file.write(", ".join(all_strengths) + "\n")
+        output_file.write("Weaknesses:\n")
+        output_file.write(", ".join(all_weaknesses) + "\n\n")
 
 if __name__ == "__main__":
     main()
